83aa15e2cdac0bfb35158d7fae6e01953e84e4d3
Danilo <danilosh45@gmail.com>
2020-06-17 19:14:04 +0200

    resolving config for deploy heroku #004

--- djreact/urls.py
+++ djreact/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('articles.api.urls')),
-    re_path('*', TemplateView.as_view(template_name='index.html'))
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
     
 ]
